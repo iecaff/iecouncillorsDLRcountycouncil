fb70be132ab3301bc9208443bdbc926b4bca1a8a
iecaff <roscaf@gmail.com>
2015-05-18 18:43:34 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -30,21 +30,10 @@ con_info = ['Area:','Ward:','Elected:','Party:','Address:','Email:','Phone:','Mo
 
 front_page = mechanize.get('http://www.dlrcoco.ie/aboutus/councilbusiness/listofcouncillors/')
 con_table = front_page.search("table[summary='List od Dun Laoghaire Rathdown Councillors in Alphabetical order']/tr")
-#con_text=con_table.map(&:text).delete_if{|x| x !~ /\w/}
-#con_text.each_with_index do |con_string,i|
-#  puts con_string
-#end
-con_details = con_table.collect do |con_row|
-  con_detail = {}
-  [
-    [:image, 'td[1]/div[1]/img/src()'],
-    [:name, 'td[2]//text()'],
-    [:phone, 'td[3]//text()'],
-    [:email, 'td[4]//text()'],
-    [:area, 'td[5]//text()'],
-  ].each do |name, xpath|
-    detail[name] = con_row.at(xpath).to_s.strip
-  end
-  con_detail
+
+con_text.each_with_index do |con_string,i|
+  con_items = con_string.search('//text()')
+  puts con_items
 end
-pp con_details
+
+
