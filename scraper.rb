c35befa284913eb1fcf28b4e0c078933eb147226
iecaff <roscaf@gmail.com>
2015-05-18 18:16:41 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -29,7 +29,7 @@ con_address = ''
 con_info = ['Area:','Ward:','Elected:','Party:','Address:','Email:','Phone:','Mobile:','Fax:',nil]
 
 front_page = mechanize.get('http://www.dlrcoco.ie/aboutus/councilbusiness/listofcouncillors/')
-con_table = front_page.search("table[class='List od Dun Laoghaire Rathdown Councillors in Alphabetical order']//text()")
+con_table = front_page.search("table[summary='List od Dun Laoghaire Rathdown Councillors in Alphabetical order']//text()")
 con_text=con_table.map(&:text).delete_if{|x| x !~ /\w/}
 con_text.each_with_index do |con_string,i|
   puts con_string
